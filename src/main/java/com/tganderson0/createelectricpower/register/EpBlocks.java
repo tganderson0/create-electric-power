ab4281bf39123e6c00c84e2fe22e3c592a79e829
Taylor Anderson <tganderson0@gmail.com>
2025-04-10 22:22:05 -0600

    Texture improvements

--- src/main/java/com/tganderson0/createelectricpower/register/EpBlocks.java
+++ src/main/java/com/tganderson0/createelectricpower/register/EpBlocks.java
@@ -1,16 +1,21 @@
 package com.tganderson0.createelectricpower.register;
 
+import com.simibubi.create.AllItems;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.data.SharedProperties;
+import com.tganderson0.createelectricpower.CreateElectricPower;
 import com.tganderson0.createelectricpower.content.electricmotor.ElectricMotorBlock;
 import com.tganderson0.createelectricpower.content.electricmotor.ElectricMotorGenerator;
 import com.tganderson0.createelectricpower.content.generator.GeneratorBlock;
 import com.tganderson0.createelectricpower.content.generator.GeneratorGenerator;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.Rarity;
 
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.tganderson0.createelectricpower.CreateElectricPower.REG;
+import static com.tterrag.registrate.providers.RegistrateRecipeProvider.has;
 
 public class EpBlocks {
 
@@ -18,6 +23,17 @@ public class EpBlocks {
             .initialProperties(SharedProperties::softMetal)
             .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
             .blockstate(new ElectricMotorGenerator()::generate)
+            .recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 1)
+                    .pattern("ELE")
+                    .pattern("CIC")
+                    .pattern("EIE")
+                    .define('E', AllItems.IRON_SHEET)
+                    .define('L', AllItems.ELECTRON_TUBE)
+                    .define('C', AllItems.COPPER_SHEET)
+                    .define('I', AllItems.ANDESITE_ALLOY)
+                    .unlockedBy("has_" + c.getName(), has(c.get()))
+                    .save(p, CreateElectricPower.loc("crafting/" + c.getName()))
+            )
             .item()
             .properties(p -> p.rarity(Rarity.UNCOMMON))
             .transform(customItemModel())
@@ -27,6 +43,17 @@ public class EpBlocks {
             .initialProperties(SharedProperties::softMetal)
             .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
             .blockstate(new GeneratorGenerator()::generate)
+            .recipe((c, p) -> ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, c.get(), 1)
+                    .pattern("ELE")
+                    .pattern("CIC")
+                    .pattern("EIE")
+                    .define('E', AllItems.STURDY_SHEET)
+                    .define('L', AllItems.ELECTRON_TUBE)
+                    .define('C', AllItems.COPPER_SHEET)
+                    .define('I', AllItems.ANDESITE_ALLOY)
+                    .unlockedBy("has_" + c.getName(), has(c.get()))
+                    .save(p, CreateElectricPower.loc("crafting/" + c.getName()))
+            )
             .item()
             .properties(p -> p.rarity(Rarity.EPIC))
             .transform(customItemModel())
